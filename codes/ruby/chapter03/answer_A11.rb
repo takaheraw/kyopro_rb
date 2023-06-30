e0d427c60b6ea5f724e0f1daf783c221e5fe1f6d
takaheraw <takaheraw@gmail.com>
2023-07-01 07:44:48 +0900

    二分探索

--- codes/ruby/chapter03/answer_A11.rb
+++ codes/ruby/chapter03/answer_A11.rb
@@ -1,24 +1,22 @@
-# 整数 x が何番目に存在するかを返す関数
-def search(x, a)
+def search(target, targets)
 	l = 0
-	r = a.length - 1
+	r = targets.length - 1
 	while l <= r
-		m = (l + r).div(2)
-		if x < a[m]
-			r = m - 1
-		elsif x == a[m]
-			return m
-		elsif x > a[m]
-			l = m + 1
+		mid_index = (l + r).div(2)
+		mid = targets[mid_index]
+		if target < mid
+			r = mid_index - 1
+		elsif target == mid
+			return mid_index
+		elsif target > mid
+			l = mid_index + 1
 		end
 	end
 	return -1 
 end
 
-# 入力（配列 X が 0 番目から始まることに注意）
-n, x = gets.split.map(&:to_i)
-a = gets.split.map(&:to_i)
+target  = gets.to_i
+targets = gets.split.map(&:to_i)
 
-# 二分探索を行う（配列が 0 番目から始まるので、答えに 1 を足している）
-answer = search(x, a)
+answer = search(target, targets)
 puts answer + 1
