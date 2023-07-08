d372fb1776c380140d48291ec405b9330c02adee
takaheraw <takaheraw@gmail.com>
2023-07-09 08:28:50 +0900

    prime check

--- codes/ruby/chapter05/answer_A26.rb
+++ codes/ruby/chapter05/answer_A26.rb
@@ -1,27 +1,24 @@
-# x が素数のとき true を、素数ではないとき false を返す
 def is_prime(n)
-	limit = (n ** 0.5).to_i
-	for i in 2..(limit + 1)
-		if n % i == 0
-			return false
-		end
-	end
-	return true
+  limit = Math.sqrt(n).to_i
+  (2..limit).each do |i|
+    return false if n % i == 0
+  end
+  return true
 end
 
+
 # 入力
-q = gets.to_i
-x = Array.new(q)
-for i in 0...q
-	x[i] = gets.to_i
+query_count = gets.to_i
+query_values = Array.new(query_count)
+for index in 0...query_count
+  query_values[index] = gets.to_i
 end
 
 # 出力
-for i in 0...q
-	answer = is_prime(x[i])
-	if answer == true
-		puts "Yes"
-	else
-		puts "No"
-	end
+query_values.each do |value|
+  if is_prime(value)
+    puts "Yes"
+  else
+    puts "No"
+  end
 end
