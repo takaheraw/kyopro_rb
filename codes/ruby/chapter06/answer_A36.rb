8233fa257b93176a5ba69a8fb2c128576d8a3d35
takaheraw <takaheraw@gmail.com>
2023-07-18 09:26:16 +0900

    Travel

--- codes/ruby/chapter06/answer_A36.rb
+++ codes/ruby/chapter06/answer_A36.rb
@@ -1,8 +1,8 @@
-# 入力
-N, K = gets.chomp.split.map(&:to_i)
+# Input
+n, k = gets.split.map(&:to_i)
 
-# 出力
-if K >= 2 * N - 2 && K % 2 == 0
+# Output
+if k >= 2 * n - 2 && k % 2 == 0
   puts "Yes"
 else
   puts "No"
