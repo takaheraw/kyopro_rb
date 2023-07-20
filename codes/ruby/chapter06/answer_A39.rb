0b472ecf9632e8737b233b7cbcba320ad65f794c
takaheraw <takaheraw@gmail.com>
2023-07-21 08:40:55 +0900

    Interval Scheduling

--- codes/ruby/chapter06/answer_A39.rb
+++ codes/ruby/chapter06/answer_A39.rb
@@ -1,24 +1,21 @@
 # 入力
-# A は (終了時刻, 開始時刻) になっていることに注意 [終了時刻の昇順にソートするため]
-N = gets.chomp.to_i
-A = []
-N.times do
-  l, r = gets.chomp.split.map(&:to_i)
-  A << [r, l]
+n = gets.to_i
+a = []
+n.times do
+  l, r = gets.split.map(&:to_i)
+  a << [r, l]
 end
 
-# ソート
-A.sort!
+a.sort!
 
-# 終了時刻の早いものから貪欲に取っていく（CurrentTime は現在時刻）
-CurrentTime = 0
-Answer = 0
-A.each do |end_time, start_time|
-  if CurrentTime <= start_time
-    CurrentTime = end_time
-    Answer += 1
+current_time = 0
+answer = 0
+a.each do |r, l|
+  if current_time <= l
+    current_time = r
+    answer += 1
   end
 end
 
 # 出力
-puts Answer
+puts answer
