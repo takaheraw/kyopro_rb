6d36b382ee8e1f2a5aa5b33d57cead5907040593
takaheraw <takaheraw@gmail.com>
2023-07-24 08:52:36 +0900

    Soccer

--- codes/ruby/chapter06/answer_A42.rb
+++ codes/ruby/chapter06/answer_A42.rb
@@ -1,32 +1,32 @@
 # 整数の組 (a, b) が決まったときの、参加可能な生徒数を返す関数
-def get_score(a, b, a_list, b_list, k)
+def get_score(a, b, a_arr, b_arr, k)
   cnt = 0
-  a_list.size.times do |i|
-    if a <= a_list[i] && a_list[i] <= a + k && b <= b_list[i] && b_list[i] <= b + k
+  a_arr.each_with_index do |value, index|
+    if a <= value && value <= a + k && b <= b_arr[index] && b_arr[index] <= b + k
       cnt += 1
     end
   end
-  cnt
+  return cnt
 end
 
 # 入力
-N, K = gets.chomp.split.map(&:to_i)
-A = []
-B = []
-N.times do
-  a, b = gets.chomp.split.map(&:to_i)
-  A << a
-  B << b
+n, k = gets.split.map(&:to_i)
+a = []
+b = []
+n.times do
+  a_val, b_val = gets.split.map(&:to_i)
+  a << a_val
+  b << b_val
 end
 
 # (a, b) の組を全探索
-Answer = 0
-(1..100).each do |a|
-  (1..100).each do |b|
-    score = get_score(a, b, A, B, K)
-    Answer = [Answer, score].max
+answer = 0
+(1..100).each do |a_val|
+  (1..100).each do |b_val|
+    score = get_score(a_val, b_val, a, b, k)
+    answer = [answer, score].max
   end
 end
 
 # 出力
-puts Answer
+puts answer
