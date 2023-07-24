94a6ae9d79084bb3494be829ad3ce5c8ce109f51
takaheraw <takaheraw@gmail.com>
2023-07-25 08:22:21 +0900

    Travel3

--- codes/ruby/chapter06/answer_A43.rb
+++ codes/ruby/chapter06/answer_A43.rb
@@ -1,22 +1,22 @@
 # 入力
-N, L = gets.chomp.split.map(&:to_i)
-A = []
-B = []
-N.times do
-  a, b = gets.chomp.split
-  A << a.to_i
-  B << b
+n, l = gets.split.map(&:to_i)
+a = []
+b = []
+n.times do
+  a_val, b_val = gets.split
+  a << a_val.to_i
+  b << b_val
 end
 
-# 答えを求める
-Answer = 0
-N.times do |i|
-  if B[i] == 'E'
-    Answer = [Answer, L - A[i]].max
-  elsif B[i] == 'W'
-    Answer = [Answer, A[i]].max
+answer = 0
+n.times do |i|
+  if b[i] == 'E'
+    answer = [answer, l - a[i]].max
+  end
+  if b[i] == 'W'
+    answer = [answer, a[i]].max
   end
 end
 
 # 出力
-puts Answer
+puts answer
