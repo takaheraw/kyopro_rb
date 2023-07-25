932292c69fe7d8a3844cad729a0256da47eaa844
takaheraw <takaheraw@gmail.com>
2023-07-26 08:34:18 +0900

    Change and Reverse

--- codes/ruby/chapter06/answer_A44.rb
+++ codes/ruby/chapter06/answer_A44.rb
@@ -1,12 +1,12 @@
 # 入力・配列の準備
-N, Q = gets.chomp.split.map(&:to_i)
+n, q = gets.split.map(&:to_i)
 state = 1
-e = [nil] * (N + 2)
-(1..N).each { |i| e[i] = i }
+e = Array.new(n + 2)
+(1..n).each { |i| e[i] = i }
 
 # クエリの処理
-Q.times do
-  query = gets.chomp.split
+q.times do
+  query = gets.split
 
   # [1] 変更操作
   if query[0].to_i == 1
@@ -15,13 +15,17 @@ Q.times do
     if state == 1
       e[x] = y
     elsif state == 2
-      e[N + 1 - x] = y
+      e[n+1-x] = y
     end
   end
 
   # [2] 反転操作
   if query[0].to_i == 2
-    state = state == 1 ? 2 : 1
+    if state == 1
+      state = 2
+    else
+      state = 1
+    end
   end
 
   # [3] 取得操作
@@ -30,7 +34,7 @@ Q.times do
     if state == 1
       puts e[x]
     elsif state == 2
-      puts e[N + 1 - x]
+      puts e[n+1-x]
     end
   end
 end
