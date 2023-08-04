6ad346ee0874722893fac48c162d1de37c969c46
takaheraw <takaheraw@gmail.com>
2023-08-04 09:43:21 +0900

    Priority Queue

--- codes/ruby/chapter08/answer_A53.rb
+++ codes/ruby/chapter08/answer_A53.rb
@@ -1,16 +1,17 @@
-# 入力
-Q = gets.chomp.to_i # クエリ数
-queries = Array.new(Q) { gets.chomp.split } # クエリの情報（各要素は ["1", 値段を表す文字列] or ["2"] or ["3"]）
+# Input
+q = gets.to_i
+queries = Array.new(q) { gets.split }
 
-# クエリの処理
-T = []
-queries.each do |q|
-  if q[0] == "1"
-    T << q[1].to_i
-    T.sort!
-  elsif q[0] == "2"
-    puts T[0] # ソート済みの配列の最初の要素が最小値
-  elsif q[0] == "3"
-    T.shift
+# Process queries
+t = []
+queries.each do |query|
+  case query[0]
+  when "1"
+    t.push(query[1].to_i)
+    t.sort!
+  when "2"
+    puts t.first
+  when "3"
+    t.shift
   end
 end
