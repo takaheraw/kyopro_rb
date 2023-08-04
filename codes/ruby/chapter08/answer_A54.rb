22047e4f5c9d4e33f764309a7b1868f178d63d4b
takaheraw <takaheraw@gmail.com>
2023-08-05 07:03:41 +0900

    Map

--- codes/ruby/chapter08/answer_A54.rb
+++ codes/ruby/chapter08/answer_A54.rb
@@ -1,13 +1,13 @@
-# 入力
-Q = gets.chomp.to_i # クエリ数
-queries = Array.new(Q) { gets.chomp.split } # クエリの情報（各要素は ["1", 名前を表す文字列, 得点を表す文字列] or ["2", 名前を表す文字列]）
+# Input
+q = gets.to_i
+queries = Array.new(q) { gets.split }
 
-# クエリの処理
-Map = Hash.new
-queries.each do |q|
-  if q[0] == "1"
-    Map[q[1]] = q[2]
-  elsif q[0] == "2"
-    puts Map[q[1]]
+map = {}
+queries.each do |query|
+  case query[0]
+  when "1"
+    map[query[1]] = query[2]
+  when "2"
+    puts map[query[1]]
   end
 end
