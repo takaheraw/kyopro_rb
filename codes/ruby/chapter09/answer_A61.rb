223e37c48bd8ab5f941d09006511efd158657da2
takaheraw <takaheraw@gmail.com>
2023-08-13 06:35:36 +0900

    Adjacent Vertices

--- codes/ruby/chapter09/answer_A61.rb
+++ codes/ruby/chapter09/answer_A61.rb
@@ -1,20 +1,18 @@
 # 入力
-N, M = gets.split.map(&:to_i)
-edges = Array.new(M) { gets.split.map(&:to_i) }
+n, m = gets.split.map(&:to_i)
+edges = Array.new(m) { gets.split.map(&:to_i) }
 
 # 隣接リストの作成
-G = Array.new(N + 1) { [] } # G[i] は頂点 i に隣接する頂点のリスト
+g = Array.new(n + 1) { [] }
 edges.each do |a, b|
-  G[a].push(b) # 頂点 a に隣接する頂点として b を追加
-  G[b].push(a) # 頂点 b に隣接する頂点として a を追加
+  g[a].push(b)
+  g[b].push(a)
 end
 
 # 出力
-(1..N).each do |i|
-  output = ''
-  output += i.to_s
-  output += ': {'
-  output += G[i].join(', ') # 例えば G[i] = { 2, 7, 5 } のとき、ここで "2, 7, 5" という文字列が output の後ろに連結される
-  output += '}'
+(1..n).each do |i|
+  output = "#{i}: {"
+  output += g[i].join(", ")
+  output += "}"
   puts output
 end
