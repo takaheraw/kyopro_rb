2eaf1696ef850259597b1a6de06d491dc33dd376
takaheraw <takaheraw@gmail.com>
2023-08-19 07:18:26 +0900

    MST

--- codes/ruby/chapter09/answer_A67.rb
+++ codes/ruby/chapter09/answer_A67.rb
@@ -1,59 +1,54 @@
-# Union-Find Tree
 class UnionFind
-  # Create a Union-Find Tree with n vertices
   def initialize(n)
     @n = n
-    @par = Array.new(n + 1, -1) # Initially, there are no parents
-    @size = Array.new(n + 1, 1) # Initially, the number of vertices in the group is 1
+    @par = Array.new(n + 1, -1)
+    @size = Array.new(n + 1, 1)
   end
 
-  # Returns the root of vertex x
   def root(x)
-    # Continue to move one step forward (parent) until there is no parent (i.e., reach the root)
-    while @par[x] != -1
-      x = @par[x]
-    end
+    x = @par[x] while @par[x] != -1
     x
   end
 
-  # Unite elements u and v
   def unite(u, v)
-    rootu = root(u)
-    rootv = root(v)
-    if rootu != rootv
-      # Process only when u and v are in different groups
-      if @size[rootu] < @size[rootv]
-        @par[rootu] = rootv
-        @size[rootv] += @size[rootu]
-      else
-        @par[rootv] = rootu
-        @size[rootu] += @size[rootv]
-      end
+    root_u = root(u)
+    root_v = root(v)
+    return if root_u == root_v
+
+    if @size[root_u] < @size[root_v]
+      @par[root_u] = root_v
+      @size[root_v] += @size[root_u]
+    else
+      @par[root_v] = root_u
+      @size[root_u] += @size[root_v]
     end
   end
 
-  # Returns whether elements u and v are in the same group
   def same(u, v)
     root(u) == root(v)
   end
 end
 
-# Input
-n, m = gets.split.map(&:to_i)
-edges = Array.new(m) { gets.split.map(&:to_i) }
+# 入力
+n, m = gets.chomp.split.map(&:to_i)
+edges = []
+(1..m).each do
+  a, b, c = gets.chomp.split.map(&:to_i)
+  edges << { a: a, b: b, c: c, idx: edges.size + 1 }
+end
 
-# Sort edges by length in ascending order
-edges.sort_by! { |x| x[2] }
+# 辺の長さの小さい順にソートする
+sorted_edges = edges.sort_by { |edge| edge[:c] }
 
-# Find the minimum spanning tree
-uf = UnionFind.new(n)
+# 最小全域木を求める
 answer = 0
-edges.each do |a, b, c|
-  unless uf.same(a, b)
-    uf.unite(a, b)
-    answer += c
+uf = UnionFind.new(n)
+sorted_edges.each do |edge|
+  if !uf.same(edge[:a], edge[:b])
+    uf.unite(edge[:a], edge[:b])
+    answer += edge[:c]
   end
 end
 
-# Output the answer
+# 答えの出力
 puts answer
