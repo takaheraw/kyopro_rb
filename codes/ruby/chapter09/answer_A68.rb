e3c0a43f66f42e52e9ce2036a8f9c81730033479
takaheraw <takaheraw@gmail.com>
2023-08-20 07:09:13 +0900

    Maximum Flow

--- codes/ruby/chapter09/answer_A68.rb
+++ codes/ruby/chapter09/answer_A68.rb
@@ -1,5 +1,4 @@
-# Structure for edges used in maximum flow
-class MaxFlowEdge
+class FlowEdge
   attr_accessor :to, :cap, :rev
 
   def initialize(to, cap, rev)
@@ -9,54 +8,53 @@ class MaxFlowEdge
   end
 end
 
-# Depth-first search
-def dfs(pos, goal, f, g, used)
-  return f if pos == goal # Arrived at the goal: The flow can be passed!
+class MaximumFlow
+  def initialize(n)
+    @n = n
+    @used = Array.new(n + 1, false)
+    @g = Array.new(n + 1) { [] }
+  end
 
-  # Explore
-  used[pos] = true
-  g[pos].each do |e|
-    # Go only to vertices with a capacity of 1 or more and not yet visited
-    if e.cap > 0 && !used[e.to]
-      flow = dfs(e.to, goal, [f, e.cap].min, g, used)
-      # If the flow can be passed, increase/decrease the capacity of the residual graph by flow
-      if flow >= 1
-        e.cap -= flow
-        g[e.to][e.rev].cap += flow
+  def add_edge(a, b, c)
+    @g[a] << FlowEdge.new(b, c, @g[b].size)
+    @g[b] << FlowEdge.new(a, 0, @g[a].size - 1)
+  end
+
+  def dfs(pos, goal, f)
+    return f if pos == goal
+
+    @used[pos] = true
+    @g[pos].each_with_index do |e, i|
+      next if e.cap == 0 || @used[e.to]
+      flow = dfs(e.to, goal, [f, e.cap].min)
+      if flow > 0
+        @g[pos][i].cap -= flow
+        @g[e.to][e.rev].cap += flow
         return flow
       end
     end
+    return 0
   end
-  # Not found after exploring all edges...
-  0
-end
 
-# Returns the total flow of the maximum flow from vertex s to vertex t (number of vertices N, list of edges)
-def maxflow(n, s, t, edges)
-  # Construct the initial residual graph
-  g = Array.new(n + 1) { [] }
-  edges.each do |a, b, c|
-    g[a] << MaxFlowEdge.new(b, c, g[b].size)
-    g[b] << MaxFlowEdge.new(a, 0, g[a].size - 1)
-  end
-  inf = 10**10
-  total_flow = 0
-  loop do
-    used = Array.new(n + 1, false)
-    f = dfs(s, t, inf, g, used)
-    if f > 0
+  def max_flow(s, t)
+    total_flow = 0
+    loop do
+      @used.fill(false)
+      f = dfs(s, t, Float::INFINITY)
+      break if f == 0
       total_flow += f
-    else
-      break # Finish operation when the flow can no longer be passed
     end
+    total_flow
   end
-  total_flow
 end
 
-# Input
-n, m = gets.split.map(&:to_i)
-edges = Array.new(m) { gets.split.map(&:to_i) }
+# 入力
+n, m = gets.chomp.split.map(&:to_i)
+z = MaximumFlow.new(n)
+m.times do
+  a, b, c = gets.chomp.split.map(&:to_i)
+  z.add_edge(a, b, c)
+end
 
-# Find and output the answer
-answer = maxflow(n, 1, n, edges)
-puts answer
+# 答えを求めて出力
+puts z.max_flow(1, n)
