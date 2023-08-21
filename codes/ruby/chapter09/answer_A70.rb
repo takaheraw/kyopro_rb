f1936b2b87b272a46316672b54334ad8d9d049c1
takaheraw <takaheraw@gmail.com>
2023-08-22 07:17:53 +0900

    Lanterns

--- codes/ruby/chapter09/answer_A70.rb
+++ codes/ruby/chapter09/answer_A70.rb
@@ -4,40 +4,30 @@ actions = Array.new(m) { gets.split.map { |x| x.to_i - 1 } }
 
 def get_next(pos, x, y, z, n)
   state = Array.new(n) { |i| (pos / (2 ** i)) % 2 }
-  state[x] = 1 - state[x]
-  state[y] = 1 - state[y]
-  state[z] = 1 - state[z]
-  ret = 0
-  state.each_with_index do |val, i|
-    ret += 2 ** i if val == 1
-  end
-  ret
+  [x, y, z].each { |lamp| state[lamp] ^= 1 }
+  state.each_with_index.sum { |s, i| s == 1 ? 2 ** i : 0 }
 end
 
-g = Array.new(2 ** n) { [] }
-(2 ** n).times do |i|
-  actions.each do |x, y, z|
-    nextstate = get_next(i, x, y, z, n)
-    g[i] << nextstate
+graph = Array.new(2 ** n) { [] }
+(0...(2 ** n)).each do |i|
+  actions.each do |action|
+    graph[i] << get_next(i, *action, n)
   end
 end
 
-start = 0
-a.each_with_index do |val, i|
-  start += 2 ** i if val == 1
-end
+start = a.each_with_index.sum { |lamp, i| lamp == 1 ? 2 ** i : 0 }
 goal = 2 ** n - 1
 
 dist = Array.new(2 ** n, -1)
 dist[start] = 0
-q = [start]
+queue = [start]
 
-until q.empty?
-  pos = q.shift
-  g[pos].each do |nex|
-    if dist[nex] == -1
-      dist[nex] = dist[pos] + 1
-      q << nex
+until queue.empty?
+  pos = queue.shift
+  graph[pos].each do |next_pos|
+    if dist[next_pos] == -1
+      dist[next_pos] = dist[pos] + 1
+      queue << next_pos
     end
   end
 end
