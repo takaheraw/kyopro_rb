a62e2d7503ccab482449885c8f49e2ace1905823
takaheraw <takaheraw@gmail.com>
2023-08-25 04:55:25 +0900

    Tile Painting

--- codes/ruby/chapter10/answer_A72.rb
+++ codes/ruby/chapter10/answer_A72.rb
@@ -1,41 +1,52 @@
-# 入力
-H, W, K = gets.split.map(&:to_i)
-c = Array.new(H) { gets.chomp }
+h, w, k = gets.split.map(&:to_i)
+grid = Array.new(h) { gets.chomp.chars }
 
-# 残り remaining_steps 回の「列に対する操作」で、最大何個のマスを黒くできるかを返す関数
-def paint_row(d, remaining_steps)
-  # 各列に対して (白マスの個数, 列の番号) の配列を記録し、大きい順にソートする
-  column = (0...W).map { |j| [d.map { |row| row[j] }.count('.'), j] }
-  column.sort!.reverse!
+class Pair
+  attr_accessor :count, :index
 
-  # 列に対して操作を行う
-  remaining_steps.times do |j|
-    idx = column[j][1]
-    H.times { |i| d[i][idx] = '#' }
+  def initialize(count, index)
+    @count = count
+    @index = index
   end
 
-  # 黒マスの個数を数えて、これを返す
-  d.map { |l| l.count('#') }.reduce(:+)
+  def <=>(other)
+    return -1 if count < other.count || (count == other.count && index < other.index)
+    return 1 if count > other.count || (count == other.count && index > other.index)
+    0
+  end
 end
 
-# 行の塗り方を全探索
-# （ここでは「ビット全探索」ではなく Array#product を使って 2^H 通りの塗り方を全列挙しています）
+def paint_column(h, w, grid_copy, remaining)
+  columns = Array.new(w) do |j|
+    cnt = 0
+    h.times { |i| cnt += 1 if grid_copy[i][j] == '.' }
+    Pair.new(cnt, j)
+  end
+
+  columns.sort!.reverse!
+
+  remaining.times do |j|
+    idx = columns[j].index
+    h.times { |i| grid_copy[i][idx] = '#' }
+  end
+
+  grid_copy.flatten.count('#')
+end
+
 answer = 0
-[0, 1].repeated_permutation(H) do |v|
-  # 行に対して操作を行う（paint_row 関数でいくつかの d[i][j] を書き換えるため、d は string の配列ではなく 2 次元リストにしています）
-  d = c.map(&:chars)
-  remaining_steps = K
-  H.times do |i|
-    if v[i] == 1
-      d[i] = ['#'] * W
-      remaining_steps -= 1
+
+(1 << h).times do |mask|
+  grid_copy = grid.map(&:dup)
+
+  remaining = k
+  h.times do |i|
+    if (mask & (1 << i)).nonzero?
+      remaining -= 1
+      w.times { |j| grid_copy[i][j] = '#' }
     end
   end
-  if remaining_steps >= 0
-    subanswer = paint_row(d, remaining_steps)
-    answer = [answer, subanswer].max
-  end
+
+  answer = [answer, paint_column(h, w, grid_copy, remaining)].max if remaining >= 0
 end
 
-# 出力
 puts answer
