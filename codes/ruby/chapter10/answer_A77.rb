576614e1157a41193d62a42f073e6c0f8421c5e3
takaheraw <takaheraw@gmail.com>
2023-08-30 06:17:07 +0900

    Yokan Party

--- codes/ruby/chapter10/answer_A77.rb
+++ codes/ruby/chapter10/answer_A77.rb
@@ -1,13 +1,13 @@
 # 入力
-N, L = gets.split.map(&:to_i)
-K = gets.to_i
-A = gets.split.map(&:to_i)
+n, l = gets.split.map(&:to_i)
+k = gets.to_i
+a = gets.split.map(&:to_i)
 
 # スコアの最大値が x 以上かを判定する関数
 def check(x, n, l, k, a)
   cnt = 0         # 現時点で何回切ったかを表す
   last_kireme = 0 # 最後どこで切ったかを表す
-  (0...n).each do |i|
+  n.times do |i|
     if a[i] - last_kireme >= x && l - a[i] >= x
       cnt += 1
       last_kireme = a[i]
@@ -16,15 +16,15 @@ def check(x, n, l, k, a)
   cnt >= k
 end
 
-# 二分探索（left: 現在の上限 ／ right: 現在の下限）
-left, right = 1, 10**9
+# 二分探索（left: 現在の下限 ／ right: 現在の上限）
+left, right = 1, 1_000_000_000
 while left < right
   mid = (left + right + 1) / 2
-  answer = check(mid, N, L, K, A)
+  answer = check(mid, n, l, k, a)
   if !answer
-    right = mid - 1 # 答えが前半部分に絞られる
+    right = mid - 1  # 答えが前半部分に絞られる
   else
-    left = mid      # 答えが後半部分に絞られる
+    left = mid       # 答えが後半部分に絞られる
   end
 end
 
