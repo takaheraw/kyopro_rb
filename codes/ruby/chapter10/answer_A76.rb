b3a35737a09bfc264252a0361f3859d3343d4bef
takaheraw <takaheraw@gmail.com>
2023-08-29 09:24:30 +0900

    River Crossing

--- codes/ruby/chapter10/answer_A76.rb
+++ codes/ruby/chapter10/answer_A76.rb
@@ -1,31 +1,27 @@
-def bisect_left(arr, x)
-  idx = arr.bsearch_index { |v| v >= x }
-  idx.nil? ? arr.length : idx
-end
-
 # 入力
-N, W, L, R = gets.split.map(&:to_i)
-X = gets.split.map(&:to_i)
+n, w, l, r = gets.split.map(&:to_i)
+x = [0] + gets.split.map(&:to_i) + [w]
 
-# 西岸を足場 0、東岸を足場 N+1 とみなす
-X = [0] + X + [W]
+# 動的計画法
+MOD = 1_000_000_007
+dp = Array.new(n + 2, 0)
+sum = Array.new(n + 2, 0)
 
-# 動的計画法（書籍内の sum[i] は本コードの dpsum[i] に対応）
-MOD = 10**9 + 7 # = 1000000007
-dp = Array.new(N + 2, 0)
-dpsum = Array.new(N + 2, 0)
 dp[0] = 1
-dpsum[0] = 1
-(1..(N + 1)).each do |i|
-  posl = bisect_left(X, X[i] - R)
-  posr = bisect_left(X, X[i] - L + 1) - 1
-  # dp[i] の値を累積和で計算（C++ とは異なり、（負の値）% MOD も 0 以上 MOD-1 以下になることに注意）
-  dp[i] = (posr >= 0 ? dpsum[posr] : 0) - (posl >= 1 ? dpsum[posl - 1] : 0)
-  dp[i] %= MOD
-  # 累積和 dpsum[i] の値を更新
-  dpsum[i] = dpsum[i - 1] + dp[i]
-  dpsum[i] %= MOD
+sum[0] = 1
+
+(1..n+1).each do |i|
+  pos_l = x.bsearch_index { |e| e >= x[i] - r } || 0
+  pos_r = x.bsearch_index { |e| e >= x[i] - l + 1 } || 0
+  pos_r -= 1
+
+  dp[i] += (pos_r >= 0 ? sum[pos_r] : 0)
+  dp[i] -= (pos_l >= 1 ? sum[pos_l - 1] : 0)
+  dp[i] = (dp[i] + MOD) % MOD
+
+  sum[i] = sum[i - 1] + dp[i]
+  sum[i] %= MOD
 end
 
 # 出力
-puts dp[N + 1]
+puts dp[n + 1]
