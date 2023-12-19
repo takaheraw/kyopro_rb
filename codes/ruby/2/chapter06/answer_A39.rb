3ea07a312deca2f2f32eaec9209bccadd0e8056c
takaheraw <takaheraw@gmail.com>
2023-12-20 06:01:40 +0900

    Interval Scheduling

---
+++ codes/ruby/2/chapter06/answer_A39.rb
@@ -0,0 +1,18 @@
+n = gets.to_i
+a = []
+n.times do
+  l, r = gets.split.map(&:to_i)
+  a << [r, l]
+end
+a.sort!
+
+current_time = 0
+answer = 0
+a.each do |r, l|
+  if current_time <= l
+    current_time = r
+    answer += 1
+  end
+end
+
+puts answer
