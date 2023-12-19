d7ee204ca88acdbeeb27c7b2f86723bad967fdf4
takaheraw <takaheraw@gmail.com>
2023-12-20 06:09:57 +0900

    Triangle

---
+++ codes/ruby/2/chapter06/answer_A40.rb
@@ -0,0 +1,14 @@
+n = gets.to_i
+a = gets.split.map(&:to_i)
+
+cnt = Array.new(101, 0)
+answer = 0
+a.each do |i|
+  cnt[i] += 1
+end
+
+(1..100).each do |i|
+  answer += cnt[i] * (cnt[i] - 1) * (cnt[i] - 2) / 6
+end
+
+puts answer
