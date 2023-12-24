8bee7a34c8525cd16b87954270c66d45c8192c59
takaheraw <takaheraw@gmail.com>
2023-12-25 06:38:57 +0900

    Card Elimination

---
+++ codes/ruby/2/chapter06/answer_A45.rb
@@ -0,0 +1,24 @@
+n, c = gets.chomp.split
+a = gets.chomp
+n = n.to_i
+
+score = 0
+n.times do |i|
+  if a[i] == 'W'
+    score += 0
+  elsif a[i] == 'B'
+    score += 1
+  elsif a[i] == 'R'
+    score += 2
+  end
+end
+
+if score % 3 == 0 && c == 'W'
+  puts 'Yes'
+elsif score % 3 == 1 && c == 'B'
+  puts 'Yes'
+elsif score % 3 == 2 && c == 'R'
+  puts 'Yes'
+else
+  puts 'No'
+end
