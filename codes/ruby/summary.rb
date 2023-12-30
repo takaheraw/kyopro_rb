49e58d6a0ed4de1f559cbc17a3ed2ace28e3855a
takaheraw <takaheraw@gmail.com>
2023-12-31 05:57:30 +0900

    Summary

--- codes/ruby/summary.rb
+++ codes/ruby/summary.rb
@@ -1,4 +1,4 @@
-# 累積和
+puts '累積和'
 visitors = [62, 65, 41, 13, 20, 11, 18, 44, 53, 12, 18, 17, 14, 10, 39]
 accumulates = Array.new(visitors.length + 1, 0)
 (1..visitors.length).each do |i|
@@ -9,7 +9,7 @@ targets.each do |l, r|
   puts accumulates[r] - accumulates[l - 1]
 end
 
-# クイックソート
+puts 'クイックソート'
 def quicksort(arr)
   return arr if arr.length <= 1
 
@@ -23,9 +23,9 @@ end
 
 array_to_sort = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
 sorted_array  = quicksort(array_to_sort)
-puts "Sorted Array: #{sorted_array}"
+puts "#{sorted_array}"
 
-# 二分探索
+puts '二分探索'
 def binary_search(arr, target)
   low  = 0
   high = arr.length - 1
@@ -45,3 +45,48 @@ end
 target = 5
 index  = binary_search(sorted_array, target)
 puts index
+
+puts '動的計画法'
+room_count = 5
+pattern1 = [2, 4, 1, 3]
+pattern2 = [5, 3, 7]
+
+dp = Array.new(room_count + 1)
+dp[1] = 0
+dp[2] = pattern1[0]
+(3..room_count).each do |i|
+  dp[i] = [dp[i - 1] + pattern1[i - 2], dp[i - 2] + pattern2[i - 3]].min
+end
+
+puts dp[room_count]
+
+puts '素数'
+
+def is_prime(n)
+  limit = Math.sqrt(n).to_i
+  (2..limit).each do |i|
+    return false if n % i == 0
+  end
+  true
+end
+puts is_prime(17)
+puts is_prime(35)
+
+puts '最大公約数'
+
+def gcd(a, b)
+  while a >= 1 && b >= 1
+    if a >= b
+      a = a % b
+    else
+      b = b % a
+    end
+  end
+
+  return a if a >= 1
+
+  b
+end
+
+puts gcd(900, 100)
+puts gcd(117, 432)
