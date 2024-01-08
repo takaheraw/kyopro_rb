271755f200893aff42009834415efe89edcd37fd
takaheraw <takaheraw@gmail.com>
2024-01-09 04:53:13 +0900

    Summary

--- codes/elixir/summary.exs
+++ codes/elixir/summary.exs
@@ -40,7 +40,37 @@ defmodule QuickSort do
   end
 end
 
-IO.puts "クイックソート"
 array_to_sort = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
 sorted_array = QuickSort.sort(array_to_sort)
 Enum.each(sorted_array, fn(s) -> IO.puts(s) end)
+
+defmodule BinarySearch do
+  def search(arr, target) do
+    binary_search(arr, target, 0, length(arr) - 1)
+  end
+
+  defp binary_search(arr, target, low, high) do
+    if low <= high do
+      mid = div(low + high, 2)
+
+      case compare(Enum.at(arr, mid), target) do
+        :equal   -> mid
+        :less    -> binary_search(arr, target, mid + 1, high)
+        :greater -> binary_search(arr, target, low, mid - 1)
+      end
+    end
+  end
+
+  defp compare(value, target) do
+    cond do
+      value == target -> :equal
+      value < target  -> :less
+      true            -> :greater
+    end
+  end
+end
+
+IO.puts "二分探索"
+target = 5
+index = BinarySearch.search(sorted_array, target)
+IO.puts index
